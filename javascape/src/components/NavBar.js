03c6a39d6db6c2e7b642127a03018527bc5a2bdc
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2022-12-13 12:33:54 +0800

    Nar Bar comment added

--- javascape/src/components/NavBar.js
+++ javascape/src/components/NavBar.js
@@ -6,28 +6,34 @@ import { MapperContext } from '../globalVariables/MapperContextProvider'
 import { NavLink } from 'react-router-dom'
 
 export default function NavBar() {
+    // call data from mapper context js
     const {
         userData,
         authUser
     } = useContext(MapperContext);
 
+    // nav bar state
     const [nav, setNav] = useState(true)
-
+    // nav bar function
     const handleNav = () => {
         setNav(!nav)
     }
 
+    // nav bar text active and inactive link style
     const activeLink = "text-white"
     const inactiveLink = "text-[#B154F0]"
 
     return (
         <div className='bg-[#19002A] w-full text-white font-exo uppercase absolute'>
             <div className='max-w-[1280px] h-[100px] mx-auto px-4 flex justify-between items-center'>
+
+                {/* Logo */}
                 <div>
                     <NavLink to="/"><img src={Logo} alt="" className="max-w-[10rem]" /></NavLink>
                 </div>
 
                 <div className='hidden md:flex items-center '>
+                    {/* Nav Text */}
                     <ul className='flex'>
                         <li className='px-5'><NavLink to="/" className={({ isActive }) => isActive ? activeLink : inactiveLink}>Home</NavLink></li>
                         <li className='px-5'><NavLink to="/topic" className={({ isActive }) => isActive ? activeLink : inactiveLink}>Topic</NavLink></li>
@@ -36,7 +42,9 @@ export default function NavBar() {
                         <li className='px-5'><NavLink to="/forum" className={({ isActive }) => isActive ? activeLink : inactiveLink}>Forum</NavLink></li>
                     </ul>
 
+                    {/* Login and Button */}
                     <div class="bg-gradient-to-r from-[#FFA9C5] to-[#FF3073]/50 p-[2px] w-fit mx-5">
+                        {/* Link to profile only user is logged in else show login button */}
                         <div>
                             {
                                 authUser === null ?
@@ -51,21 +59,22 @@ export default function NavBar() {
                             }
                         </div>
                     </div>
-
-
-
                 </div>
 
+                {/* Mobile Responsive Burger Tag */}
                 <div onClick={handleNav} className="block md:hidden">
                     {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
                 </div>
             </div>
 
+            {/* Mobile Responsive Menu  */}
             <div className={!nav ? 'fixed left-0 top-0 w-[50%] bg-[#19002A] h-full ease-in-out duration-500' : 'fixed left-[-100%]'}>
+                {/* Logo */}
                 <div className='mx-[1rem] my-[0.9rem]'>
                     <img src={Logo} alt="" className="max-w-[10rem]" />
                 </div>
 
+                {/* Nav Text */}
                 <ul className="pt-5 px-5">
                     <li className='p-5 border-b border-gray-500'>Home</li>
                     <li className='p-5 border-b border-gray-500'>Topics</li>
@@ -74,7 +83,9 @@ export default function NavBar() {
                     <li className='p-5 border-b border-gray-500'>Forum</li>
                 </ul>
 
+                {/* Login and Button */}
                 <div class="bg-gradient-to-r from-[#FFA9C5] to-[#FF3073]/50 p-[2px] w-fit my-7 mx-[27px]">
+                    {/* Link to profile only user is logged in else show login button */}
                     <div>
                         {
                             authUser === null ?
