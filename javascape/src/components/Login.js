3e787248fdf2f5fd80e1d3abf13ad89f80dca70a
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2022-12-13 19:55:34 +0800

    sd4 234

--- javascape/src/components/Login.js
+++ javascape/src/components/Login.js
@@ -9,7 +9,10 @@ import { MapperContext } from '../globalVariables/MapperContextProvider'
 
 export default function Login() {
     // Get user data from context
-    const { userArray } = useContext(MapperContext);
+    const {
+        userArray,
+        authUser
+    } = useContext(MapperContext);
 
     // Login variables
     const loginEmailOrUsername = useRef("");
@@ -61,36 +64,37 @@ export default function Login() {
     }, []);
 
     return (
-        <div className='Login w-full flex flex-col justify-center items-center h-screen bg-background bg-[#09002B] text-white font-exo'>
-            {/* Logo */}
-            <div>
-                <NavLink to="/"><img src={Logo} alt="" className="max-w-[13rem] sm:max-w-[13rem] md:max-w-[17rem] lg:max-w-[17rem] my-10" /></NavLink>
-            </div>
-            {/* Login form */}
-            <div className='flex flex-col max-w-[22rem] sm:max-w-[22rem] md:max-w-[28rem] lg:max-w-[28rem] w-full items-center px-[4rem] sm:px-[4rem] md:px-[5.5rem] lg:px-[5.5rem] rounded-2xl border-2 bg-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30'>
-                {/* Login text */}
-                <span className='font-bold text-2xl text-white my-5'>Login</span>
-                {/* Email field */}
-                <div className='my-3 flex flex-col w-full'>
-                    <span>Email Address / Username: </span>
-                    <input ref={loginEmailOrUsername} type="email" required className="border-l-0 border-b-2 border-r-0 border-t-0 bg-transparent focus:outline-none" />
+        authUser !== null ? navigate("/profile") :
+            <div className='Login w-full flex flex-col justify-center items-center h-screen bg-background bg-[#09002B] text-white font-exo'>
+                {/* Logo */}
+                <div>
+                    <NavLink to="/"><img src={Logo} alt="" className="max-w-[13rem] sm:max-w-[13rem] md:max-w-[17rem] lg:max-w-[17rem] my-10" /></NavLink>
                 </div>
-                {/* Password field */}
-                <div className='my-3 flex flex-col w-full'>
-                    <span>Password: </span>
-                    <input ref={loginPassword} type="password" required className="border-l-0 border-b-2 border-r-0 border-t-0 bg-transparent focus:outline-none" />
-                </div>
-                {/* Login button */}
-                <div class="bg-gradient-to-r from-[#FFA9C5] to-[#FF3073]/50 p-[2px] my-3 max-w-[7rem] w-full">
-                    <div>
-                        <button onClick={Login} className='w-full h-[3rem] bg-[#371152] hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30'>Login</button>
+                {/* Login form */}
+                <div className='flex flex-col max-w-[22rem] sm:max-w-[22rem] md:max-w-[28rem] lg:max-w-[28rem] w-full items-center px-[4rem] sm:px-[4rem] md:px-[5.5rem] lg:px-[5.5rem] rounded-2xl border-2 bg-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30'>
+                    {/* Login text */}
+                    <span className='font-bold text-2xl text-white my-5'>Login</span>
+                    {/* Email field */}
+                    <div className='my-3 flex flex-col w-full'>
+                        <span>Email Address / Username: </span>
+                        <input ref={loginEmailOrUsername} type="email" required className="border-l-0 border-b-2 border-r-0 border-t-0 bg-transparent focus:outline-none" />
+                    </div>
+                    {/* Password field */}
+                    <div className='my-3 flex flex-col w-full'>
+                        <span>Password: </span>
+                        <input ref={loginPassword} type="password" required className="border-l-0 border-b-2 border-r-0 border-t-0 bg-transparent focus:outline-none" />
+                    </div>
+                    {/* Login button */}
+                    <div class="bg-gradient-to-r from-[#FFA9C5] to-[#FF3073]/50 p-[2px] my-3 max-w-[7rem] w-full">
+                        <div>
+                            <button onClick={Login} className='w-full h-[3rem] bg-[#371152] hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30'>Login</button>
+                        </div>
+                    </div>
+                    {/* Link to sign up view */}
+                    <div className='my-3 text-sm sm:text-sm md:text-md lg:text-md'>
+                        <span>Do not have a account? <a href='/sign-up' className='underline'>Sign Up</a></span>
                     </div>
                 </div>
-                {/* Link to sign up view */}
-                <div className='my-3 text-sm sm:text-sm md:text-md lg:text-md'>
-                    <span>Do not have a account? <a href='/sign-up' className='underline'>Sign Up</a></span>
-                </div>
             </div>
-        </div>
     )
 }
--- javascape/src/components/SignUp.js
+++ javascape/src/components/SignUp.js
@@ -1,51 +1,43 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from 'react'
+import React, { useEffect, useContext, useRef } from 'react'
 import { createUserWithEmailAndPassword } from "firebase/auth"
 import { auth, firestore } from "../firebase"
 import { doc, setDoc } from "firebase/firestore"
 import Logo from '../images/Logo.png'
 import { useNavigate } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
+import { MapperContext } from '../globalVariables/MapperContextProvider'
 
 export default function SignUp() {
+    // call data from mapper context js
+    const {
+        authUser
+    } = useContext(MapperContext)
 
-    // Register variables and set function
-    var registerEmail = null, registerPassword = null, registerUsername = null, registerPhoneNumber = null;
-
-    const setRegisterEmail = (email) => {
-        registerEmail = email;
-    }
-
-    const setRegisterPassword = (password) => {
-        registerPassword = password;
-    }
-
-    const setRegisterUsername = (username) => {
-        registerUsername = username;
-    }
-
-    const setRegisterPhoneNumber = (phone) => {
-        registerPhoneNumber = phone;
-    }
+    // Register variables
+    const registerEmail = useRef("")
+    const registerPassword = useRef("")
+    const registerUsername = useRef("")
+    const registerPhoneNumber = useRef("")
 
     // Navigate function
     const navigate = useNavigate();
 
     // Register function
     const Register = async () => {
+        console.log(registerEmail.current.value, registerPassword.current.value, registerUsername.current.value, registerPhoneNumber.current.value)
         try {
-            const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
+            const user = await createUserWithEmailAndPassword(auth, registerEmail.current.value, registerPassword.current.value)
 
             await setDoc(doc(firestore, "Users", user.user.uid), {
-                Username: registerUsername,
-                Email: registerEmail,
-                Password: registerPassword,
-                PhoneNumber: registerPhoneNumber,
+                Username: registerUsername.current.value,
+                Email: registerEmail.current.value,
+                Password: registerPassword.current.value,
+                PhoneNumber: registerPhoneNumber.current.value,
                 CreateDate: new Date(),
             })
 
-            navigate('/profile')
-
+            window.location.reload();
         } catch (error) {
             console.log('Error in creating user', error);
         }
@@ -70,46 +62,47 @@ export default function SignUp() {
 
 
     return (
-        <div className='SignUp flex flex-col justify-center items-center h-screen bg-background bg-[#09002B] text-white font-exo'>
-            {/* Logo */}
-            <div>
-                <NavLink to="/"><img src={Logo} alt="" className="max-w-[13rem] sm:max-w-[13rem] md:max-w-[17rem] lg:max-w-[17rem] my-10" /></NavLink>
-            </div>
-            {/* Sign up form */}
-            <div className='flex flex-col max-w-[22rem] sm:max-w-[22rem] md:max-w-[28rem] lg:max-w-[28rem] w-full items-center px-[4rem] sm:px-[4rem] md:px-[5.5rem] lg:px-[5.5rem] rounded-2xl border-2 bg-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30'>
-                {/* Sign up text */}
-                <span className='font-bold text-2xl text-white my-5'>Sign Up</span>
-                {/* Email field */}
-                <div className='my-3 flex flex-col w-full'>
-                    <span>Email Address: </span>
-                    <input onChange={(email) => { setRegisterEmail(email.target.value) }} type="email" required className="border-l-0 border-b-2 border-r-0 border-t-0 bg-transparent focus:outline-none" />
+        authUser !== null ? navigate("/profile") :
+            <div className='SignUp flex flex-col justify-center items-center h-screen bg-background bg-[#09002B] text-white font-exo'>
+                {/* Logo */}
+                <div>
+                    <NavLink to="/"><img src={Logo} alt="" className="max-w-[13rem] sm:max-w-[13rem] md:max-w-[17rem] lg:max-w-[17rem] my-10" /></NavLink>
                 </div>
-                {/* Phone number field */}
-                <div className='my-3 flex flex-col w-full'>
-                    <span>Phone Number: </span>
-                    <input onChange={(phone) => { setRegisterPhoneNumber(phone.target.value) }} type="phone" required className="border-l-0 border-b-2 border-r-0 border-t-0 bg-transparent focus:outline-none" />
-                </div>
-                {/* Username field */}
-                <div className='my-3 flex flex-col w-full'>
-                    <span>Username: </span>
-                    <input onChange={(username) => { setRegisterUsername(username.target.value) }} type="username" required className="border-l-0 border-b-2 border-r-0 border-t-0 bg-transparent focus:outline-none" />
-                </div>
-                {/* Password field */}
-                <div className='my-3 flex flex-col w-full'>
-                    <span>Password: </span>
-                    <input onChange={(password) => { setRegisterPassword(password.target.value) }} type="password" required className="border-l-0 border-b-2 border-r-0 border-t-0 bg-transparent focus:outline-none" />
-                </div>
-                {/* Sign up button */}
-                <div class="bg-gradient-to-r from-[#FFA9C5] to-[#FF3073]/50 p-[2px] my-3 max-w-[7rem] w-full">
-                    <div>
-                        <button onClick={Register} className='w-full h-[3rem] bg-[#371152] hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30'>Sign Up</button>
+                {/* Sign up form */}
+                <div className='flex flex-col max-w-[22rem] sm:max-w-[22rem] md:max-w-[28rem] lg:max-w-[28rem] w-full items-center px-[4rem] sm:px-[4rem] md:px-[5.5rem] lg:px-[5.5rem] rounded-2xl border-2 bg-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30'>
+                    {/* Sign up text */}
+                    <span className='font-bold text-2xl text-white my-5'>Sign Up</span>
+                    {/* Email field */}
+                    <div className='my-3 flex flex-col w-full'>
+                        <span>Email Address: </span>
+                        <input ref={registerEmail} type="email" required className="border-l-0 border-b-2 border-r-0 border-t-0 bg-transparent focus:outline-none" />
+                    </div>
+                    {/* Phone number field */}
+                    <div className='my-3 flex flex-col w-full'>
+                        <span>Phone Number: </span>
+                        <input ref={registerPhoneNumber} type="phone" required className="border-l-0 border-b-2 border-r-0 border-t-0 bg-transparent focus:outline-none" />
+                    </div>
+                    {/* Username field */}
+                    <div className='my-3 flex flex-col w-full'>
+                        <span>Username: </span>
+                        <input ref={registerUsername} type="username" required className="border-l-0 border-b-2 border-r-0 border-t-0 bg-transparent focus:outline-none" />
+                    </div>
+                    {/* Password field */}
+                    <div className='my-3 flex flex-col w-full'>
+                        <span>Password: </span>
+                        <input ref={registerPassword} type="password" required className="border-l-0 border-b-2 border-r-0 border-t-0 bg-transparent focus:outline-none" />
+                    </div>
+                    {/* Sign up button */}
+                    <div class="bg-gradient-to-r from-[#FFA9C5] to-[#FF3073]/50 p-[2px] my-3 max-w-[7rem] w-full">
+                        <div>
+                            <button onClick={Register} className='w-full h-[3rem] bg-[#371152] hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30'>Sign Up</button>
+                        </div>
+                    </div>
+                    {/* Link to login view */}
+                    <div className='my-3 text-sm sm:text-sm md:text-md lg:text-md'>
+                        <span>Already have a account? <a href='/login' className='underline'>Login</a></span>
                     </div>
                 </div>
-                {/* Link to login view */}
-                <div className='my-3 text-sm sm:text-sm md:text-md lg:text-md'>
-                    <span>Already have a account? <a href='/' className='underline'>Login</a></span>
-                </div>
             </div>
-        </div>
     )
 }
