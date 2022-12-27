4ed3c5488f5ddbc6700203e1ce05cd8732d75329
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2022-12-27 20:02:46 +0800

    Level01 Page

--- javascape/src/components/Stage.js
+++ javascape/src/components/Stage.js
@@ -7,6 +7,7 @@ import { updateDoc, doc } from 'firebase/firestore'
 import { MapperContext } from "../globalVariables/MapperContextProvider";
 import { useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
+import { NavLink } from "react-router-dom";
 
 export default function Stage() {
   // get user data from context
@@ -51,11 +52,25 @@ export default function Stage() {
 
   return (
     authUser === null ? navigate("/login") :
-      <div className="Stage bg-[#09002B] bg-background flex flex-col text-white font-exo w-full">
+      <div>
         <NavBar />
-        <div className="w-full h-screen flex justify-center items-center">
-          <Unity unityProvider={unityProvider} className="w-full max-w-[1280px]" />
+        <div className="Stage bg-[#09002B] bg-background flex flex-col text-white font-exo w-full">
+          <div className="w-full h-screen flex flex-col justify-center items-center px-4 pt-4 sm:px-4 sm:pt-4 md:px-24 md:pt-24 lg:px-24 lg:pt-24">
+            <div className="w-full max-w-[1280px] text-3xl font-bold flex justify-between mb-3">
+              <span className="text-sm sm:text-sm md:text-xl lg:text-3xl">Level 01</span>
+              <span className="text-sm sm:text-sm md:text-xl lg:text-3xl">Highest Score:</span>
+            </div>
+            <Unity unityProvider={unityProvider} className="w-full max-w-[1280px]" />
+            <div className="w-full max-w-[1280px] pt-5">
+              <div class="bg-gradient-to-r from-[#FFA9C5] to-[#FF3073]/50 p-[2px] w-fit self-end">
+                <div>
+                  <button className='text-[7px] sm:text-[7px] md:text-[10px] lg:text-[16px] px-3 h-[2rem] sm:h-[2rem] md:h-[2.6rem] lg:h-[2.6rem] bg-[#371152] hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 font-extrabold uppercase'><NavLink to="/login">Back To Menu</NavLink></button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+
   );
 }
