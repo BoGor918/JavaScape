547bd8a8ddb91d938590a5e98369cc30fbe5255f
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2022-12-28 17:01:42 +0800

    Update Battle.js

--- javascape/src/components/Battle.js
+++ javascape/src/components/Battle.js
@@ -34,27 +34,28 @@ export default function Battle() {
         <div className='Home bg-[#09002B] bg-background flex flex-col text-white font-exo w-full'>
             <NavBar />
             {/* Level Slider */}
-            <div className='max-w-[1366px] h-[950px] w-full m-auto pt-40 px-[50px]'>
-                <div>
+
+            <div className='flex justify-center items-center'>
+                <div className='w-full max-w-[1280px] pt-40 px-[50px]'>
                     <img src={`${slides[currentIndex].url}`} alt='Level Banner' />
-                    <div class="bg-gradient-to-r from-[#FFA9C5] to-[#FF3073]/50 p-[2px] w-fit absolute lg:translate-x-[85%]  lg:-translate-y-[330%]">
+                    <div class="bg-gradient-to-r from-[#FFA9C5] to-[#FF3073]/50 p-[2px] w-fit absolute translate-x-[70%] -translate-y-[280%]">
                         <div>
                             <button className='text-[7px] sm:text-[7px] md:text-[10px] lg:text-[25px] px-8 h-[2rem] sm:h-[2rem] md:h-[2.6rem] lg:h-[4rem] bg-[#371152] hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 font-extrabold uppercase'>
                                 <NavLink to={`/battle/level01/${currentUserDataSet[0]}`}>Start Level 01</NavLink>
                             </button>
                         </div>
                     </div>
-                </div>
-
-                {/* Left Arrow */}
-                <div className='absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-3xl text-white cursor-pointer'>
-                    <BsChevronCompactLeft onClick={prevSlide} size={30} />
-                </div>
-                {/* Right Arrow */}
-                <div className='absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 text-3xl text-white cursor-pointer'>
-                    <BsChevronCompactRight onClick={nextSlide} size={30} />
+                    {/* Left Arrow */}
+                    <div className='absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-3xl text-white cursor-pointer'>
+                        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+                    </div>
+                    {/* Right Arrow */}
+                    <div className='absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 text-3xl text-white cursor-pointer'>
+                        <BsChevronCompactRight onClick={nextSlide} size={30} />
+                    </div>
                 </div>
             </div>
+
 
             <Footer />
         </div>
