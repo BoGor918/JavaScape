fb5468b4d93b9d055c913134e2ab76e11604f649
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2022-12-29 22:01:43 +0800

    Hello

--- javascape/src/components/App.js
+++ javascape/src/components/App.js
@@ -37,7 +37,7 @@ export default function App() {
             <Route path={"/battle/level01/" + currentUserDataSet[1]} element={<Level01 />} /> :
             <></>
         }
-        {/* All User Profile Link */}
+        {/* all User Profile Link */}
         {
           userData.map((user) => {
             return (
