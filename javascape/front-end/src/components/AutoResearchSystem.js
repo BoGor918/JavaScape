309dc69c9f99d5d749fa4a8b52c83dc550b89c40
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2023-02-03 00:55:36 +0800

    AutoResearch done

--- javascape/front-end/src/components/AutoResearchSystem.js
+++ javascape/front-end/src/components/AutoResearchSystem.js
@@ -1,18 +1,26 @@
+/* eslint-disable array-callback-return */
+import NavBar from './NavBar'
 import React, { useEffect, useState } from "react";
 import axios from "./Utils/axios";
 import { useLocation } from "react-router-dom"
 
 export default function AutoResearchSystem() {
+    // Get query from URL
     const location = useLocation()
+    // set params
     const params = new URLSearchParams(location.search)
+    // get query value 
     const query = params.get("question")
+    // set data for printing from back-end
     const [data, setData] = useState([]);
 
+    // search function for getting query data from back-end to front-end
     const search = async (query) => {
         const { data } = await axios.get("/querySearch?q=" + query);
         return data;
     };
 
+    // calling the search function
     useEffect(() => {
         search(query).then((data) => {
             setData(data);
@@ -20,17 +28,31 @@ export default function AutoResearchSystem() {
     }, [query]);
 
     return (
-        <div>
-            {data.map((item, i) => {
-                return (
-                    <div key={i}>
-                        <div>{i + 1}</div>
-                        <div>{item.title}</div>
-                        <div>{item.description}</div>
-                        <div className="mb-10">{item.link}</div>
+        <div className='Forum bg-[#09002B] bg-background text-white font-exo h-screen'>
+            <NavBar />
+            {/* Content */}
+            <div className='w-full h-full overflow-auto flex flex-col items-center'>
+                {/* Title */}
+                <span className='text-center mt-[10rem] sm:mt-[10rem] lg:mt-[13rem] mb-[3rem] sm:mb-[3rem] lg:mb-[6rem] text-[1.7rem] sm:text-[1.7rem] md:text-[2rem] lg:text-[2.5rem] uppercase font-extrabold text-[#B154F0]'>Auto Research Topic: {params.get("question")}</span>
+                {/* Results Column */}
+                <div className='w-full flex flex-col justify-center items-center'>
+                    {/* All Results View */}
+                    <div className='w-full flex flex-col justify-center items-center'>
+                        <div className='flex flex-col max-w-[21rem] sm:max-w-[21rem] md:max-w-[45rem] lg:max-w-[80rem] mb-[3rem] sm:mb-[3rem] lg:mb-[6rem] w-full rounded-2xl border-2 bg-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 py-5 px-[20px] sm:px-[20px] md:px-[35px] lg:px-[50px]'>
+                            {data.map((item, i) => {
+                                return (
+                                    <div key={i}>
+                                        <div className='font-bold underline'>Result {i + 1}</div>
+                                        <div>Title: {item.title}</div>
+                                        <div>Description: {item.description}</div>
+                                        <div className="mb-10 truncate ...">URL: <a href={item.link} target="_blank" className="no-underline hover:underline" rel="noreferrer">{item.link}</a></div>
+                                    </div>
+                                )
+                            })}
+                        </div>
                     </div>
-                )
-            })}
+                </div>
+            </div>
         </div>
-    );
+    )
 }
