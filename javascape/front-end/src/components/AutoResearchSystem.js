a52f704257acd2f337cddd64244dd1a5a5a42a4e
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2023-02-03 10:12:36 +0800

    Auto research space remove

--- javascape/front-end/src/components/AutoResearchSystem.js
+++ javascape/front-end/src/components/AutoResearchSystem.js
@@ -33,7 +33,7 @@ export default function AutoResearchSystem() {
             {/* Content */}
             <div className='w-full h-full overflow-auto flex flex-col items-center'>
                 {/* Title */}
-                <span className='text-center mt-[10rem] sm:mt-[10rem] lg:mt-[13rem] mb-[3rem] sm:mb-[3rem] lg:mb-[6rem] text-[1.7rem] sm:text-[1.7rem] md:text-[2rem] lg:text-[2.5rem] uppercase font-extrabold text-[#B154F0]'>Auto Research Topic: {params.get("question")}</span>
+                <span className='text-center mt-[10rem] sm:mt-[10rem] lg:mt-[13rem] mb-[3rem] sm:mb-[3rem] lg:mb-[6rem] text-[1.7rem] sm:text-[1.7rem] md:text-[2rem] lg:text-[2.5rem] uppercase font-extrabold text-[#B154F0]'>Auto Research Topic: {params.get("question").replace(/-/g, ' ')}</span>
                 {/* Results Column */}
                 <div className='w-full flex flex-col justify-center items-center'>
                     {/* All Results View */}
