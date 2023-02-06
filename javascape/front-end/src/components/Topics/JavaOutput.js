164d361ba76a931642246e902bda9a0a8eddc63a
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2023-02-07 01:41:33 +0800

    Update JavaOutput.js

--- javascape/front-end/src/components/Topics/JavaOutput.js
+++ javascape/front-end/src/components/Topics/JavaOutput.js
@@ -29,6 +29,22 @@ export default function JavaOutput() {
                     <button className='px-3 h-[2.6rem] bg-[#371152] hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 font-extrabold uppercase'>Try Yourself</button>
                 </div>
             </div>
+            <hr className="w-full h-[1px] bg-white rounded border-0 opacity-70 my-6" />
+            <span className="font-bold text-xl">Double Quotes</span>
+            <span className="my-3">When you are working with text, it must be wrapped inside double quotations marks <span className="bg-white text-[#DD4A68] px-1">" "</span> .</span>
+            <span className="mb-3">If you forget the double quotes, an error occurs:</span>
+            <div className="bg-black/20 rounded flex flex-col p-5">
+                <span className="text-lg">Example</span>
+                <div className="flex flex-col bg-white text-black border-l-[5px] border-l-[#b962f4] p-3 my-5">
+                    <span><span className="text-[#DD4A68]">System</span>.out.<span className="text-[#DD4A68]">println</span><span className="text-[#690]">("This sentence will work!");</span></span>
+                </div>
+                <div className="flex flex-col bg-white text-black border-l-[5px] border-l-[#DD4A68] p-3 mb-5">
+                    <span><span className="text-[#DD4A68]">System</span>.out.<span className="text-[#DD4A68]">println</span><span>(This sentence will produce an error);</span></span>
+                </div>
+                <div className="bg-gradient-to-r from-[#FFA9C5] to-[#FF3073]/50 p-[2px] w-fit">
+                    <button className='px-3 h-[2.6rem] bg-[#371152] hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 font-extrabold uppercase'>Try Yourself</button>
+                </div>
+            </div>
         </div>
     )
 }
