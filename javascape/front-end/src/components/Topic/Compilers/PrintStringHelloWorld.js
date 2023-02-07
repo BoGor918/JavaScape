807623d8a37e6ba5e9279b66b0f6c689e40a9fc4
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2023-02-07 17:40:25 +0800

    sdfsdfsdf

--- javascape/front-end/src/components/Topic/Compilers/PrintStringHelloWorld.js
+++ javascape/front-end/src/components/Topic/Compilers/PrintStringHelloWorld.js
@@ -10,7 +10,7 @@ export default function PrintStringHelloWorld() {
   return (
     <div className='w-full flex justify-center items-center'>
       {/* Description View */}
-      <div className='flex flex-col items-center h-full mr-10 max-w-[25rem] w-full rounded-2xl border-2 bg-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 p-5'>
+      <div className='flex flex-col items-center h-full mr-4 max-w-[25rem] w-full rounded-2xl border-2 bg-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 p-5'>
         <span className='font-bold underline'>Description - Click To Copy</span>
         <CopyToClipboard text={`System.out.println("Hello World!");`} onCopy={() => setCopied(true)}>
           <button><SyntaxHighlighter className="flex flex-col bg-white text-black border-l-[5px] border-l-[#b962f4] p-3 my-5 text-left" language="java" style={docco}>{`// Print Out string values Hello World!\nSystem.out.println("Hello World!");`}</SyntaxHighlighter></button>
@@ -18,7 +18,7 @@ export default function PrintStringHelloWorld() {
         {copied ? <span className='text-white bg-[#68cb9b] px-2 rounded-sm'>✔ Copied.</span> : null}
       </div>
       {/* Compiler View*/}
-      <div className='flex flex-col h-full items-center mr-10 max-w-[70rem] w-full rounded-2xl border-2 bg-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 p-[38px]'>
+      <div className='flex flex-col h-full items-center max-w-[70rem] w-full rounded-2xl border-2 bg-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 p-[38px]'>
         <iframe className='w-full h-[30rem] rounded-md' src="https://www.interviewbit.com/embed/snippet/5a7d7fcd97fc31e7c56e" title='Interviewbit Ide snippet/173e0ccff5920b5109f5' loading="lazy" allow="clipboard-write" allowfullscreen referrerpolicy="unsafe-url" sandbox="allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation allow-popups-to-escape-sandbox"></iframe>
       </div>
     </div>
