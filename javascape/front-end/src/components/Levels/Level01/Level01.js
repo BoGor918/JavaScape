9dd85a125cb0ceac7373b9b1bf318298a0bd9d10
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2023-02-12 22:08:58 +0800

    Added

--- javascape/front-end/src/components/Levels/Level01/Level01.js
+++ javascape/front-end/src/components/Levels/Level01/Level01.js
@@ -6,7 +6,6 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 import { firestore } from "../../../firebase"
 import { updateDoc, doc, collection, getDocs } from 'firebase/firestore'
 import { MapperContext } from "../../../globalVariables/MapperContextProvider";
-import { useNavigate } from "react-router-dom";
 import NavBar from "../../NavBar";
 
 export default function Level01() {
@@ -15,9 +14,6 @@ export default function Level01() {
     authUser,
     currentUserDataSet,
   } = useContext(MapperContext);
-
-  // redirect to login page if user is not login
-  const navigate = useNavigate();
 
   // data set receive from unity
   const [score, setScore] = useState();
@@ -42,7 +38,7 @@ export default function Level01() {
       currentLevelDataSet.push(level1.Level, level1.Username, level1.HighestScore)
     }
   })
-  
+
   // unity context
   const { unityProvider, addEventListener, removeEventListener } =
     useUnityContext({
@@ -78,35 +74,34 @@ export default function Level01() {
   }, [addEventListener, removeEventListener, handleGameOver]);
 
   return (
-    authUser === null ? navigate("/login") :
-      <div>
-        <NavBar />
-        <div className="Stage flex flex-col text-white font-exo w-full">
-          <div className="w-full h-screen flex flex-col justify-center items-center px-4 pt-4 sm:px-4 sm:pt-4 md:px-24 md:pt-24 lg:px-24 lg:pt-24">
-            {
-              userLevel1Data.map((level1, i) => {
-                if (level1.Level === 1) {
-                  return (
-                    <div key={i} className="w-full max-w-[1280px] text-3xl font-bold flex justify-between mb-3">
-                      <span className="text-sm sm:text-sm md:text-xl lg:text-3xl">Level 01</span>
-                      <span className="text-sm sm:text-sm md:text-xl lg:text-3xl">Highest Score: {level1.HighestScore}</span>
-                    </div>
-                  )
-                }
-              })
-            }
-            {/* Unity Game Content */}
-            <Unity unityProvider={unityProvider} className="w-full max-w-[1280px]" />
-            {/* Exit Button */}
-            <div className="w-full max-w-[1280px] pt-5">
-              <div className="bg-gradient-to-r from-[#FFA9C5] to-[#FF3073]/50 p-[2px] w-fit self-end">
-                <div>
-                  <button onClick={() => window.close()} className='text-[7px] sm:text-[7px] md:text-[10px] lg:text-[16px] px-3 h-[2rem] sm:h-[2rem] md:h-[2.6rem] lg:h-[2.6rem] bg-[#371152] hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 font-extrabold uppercase'>Leave Game</button>
-                </div>
+    <div>
+      <NavBar />
+      <div className="Stage flex flex-col text-white font-exo w-full">
+        <div className="w-full h-screen flex flex-col justify-center items-center px-4 pt-4 sm:px-4 sm:pt-4 md:px-24 md:pt-24 lg:px-24 lg:pt-24">
+          {
+            userLevel1Data.map((level1, i) => {
+              if (level1.Level === 1) {
+                return (
+                  <div key={i} className="w-full max-w-[1280px] text-3xl font-bold flex justify-between mb-3">
+                    <span className="text-sm sm:text-sm md:text-xl lg:text-3xl">Level 01</span>
+                    <span className="text-sm sm:text-sm md:text-xl lg:text-3xl">Highest Score: {level1.HighestScore}</span>
+                  </div>
+                )
+              }
+            })
+          }
+          {/* Unity Game Content */}
+          <Unity unityProvider={unityProvider} className="w-full max-w-[1280px]" />
+          {/* Exit Button */}
+          <div className="w-full max-w-[1280px] pt-5">
+            <div className="bg-gradient-to-r from-[#FFA9C5] to-[#FF3073]/50 p-[2px] w-fit self-end">
+              <div>
+                <button onClick={() => window.close()} className='text-[7px] sm:text-[7px] md:text-[10px] lg:text-[16px] px-3 h-[2rem] sm:h-[2rem] md:h-[2.6rem] lg:h-[2.6rem] bg-[#371152] hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 font-extrabold uppercase'>Leave Game</button>
               </div>
             </div>
           </div>
         </div>
       </div>
+    </div>
   );
 }
--- javascape/front-end/src/components/Levels/Level01/Level01Intro.js
+++ javascape/front-end/src/components/Levels/Level01/Level01Intro.js
@@ -20,31 +20,37 @@ export default function Battle() {
     };
 
     return (
-        authUser === null ? window.location.replace("/login") :
-            <div className='Level01Intro flex flex-col text-white font-exo w-full'>
-                <NavBar />
-                <div className='flex flex-col justify-center items-center'>
-                    {/* Level Banner */}
-                    <div className='w-full max-w-[350px] sm:max-w-[350px] md:max-w-[700px] lg:max-w-[1280px] relative block group mt-[130px] lg:mt-40'>
-                        <img src={Level01IntroBanner} alt="" className="object-cover w-full group-hover:opacity-50 duration-500" />
-                        <div className='opacity-0 duration-500 group-hover:opacity-100'>
-                            <div style={hoverButton} className="absolute bg-gradient-to-r from-[#FFA9C5] to-[#FF3073]/50 p-[2px] my-3 max-w-[7rem] sm:max-w-[7rem] md:max-w-[13rem]  lg:max-w-[17rem] w-full">
-                                <div>
-                                    <NavLink to={`/battle/level01/${currentUserDataSet[1]}`} target="_blank"><button className='text-sm sm:text-sm md:text-[22px] lg:text-md w-full h-[3rem] sm:h-[3rem] md:h-[5rem] lg:h-[5rem] bg-[#371152] hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 uppecase font-extrabold'>Start Game</button></NavLink>
-                                </div>
+        <div className='Level01Intro flex flex-col text-white font-exo w-full'>
+            <NavBar />
+            <div className='flex flex-col justify-center items-center'>
+                {/* Level Banner */}
+                <div className='w-full max-w-[350px] sm:max-w-[350px] md:max-w-[700px] lg:max-w-[1280px] relative block group mt-[130px] lg:mt-40'>
+                    <img src={Level01IntroBanner} alt="" className="object-cover w-full group-hover:opacity-50 duration-500" />
+                    <div className='opacity-0 duration-500 group-hover:opacity-100'>
+                        <div style={hoverButton} className="absolute bg-gradient-to-r from-[#FFA9C5] to-[#FF3073]/50 p-[2px] my-3 max-w-[7rem] sm:max-w-[7rem] md:max-w-[13rem]  lg:max-w-[17rem] w-full">
+                            <div>
+                                {
+                                    authUser === null
+                                        ?
+                                        <NavLink to={`/login`}><button className='text-sm sm:text-sm md:text-[22px] lg:text-md w-full h-[3rem] sm:h-[3rem] md:h-[5rem] lg:h-[5rem] bg-[#371152] hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 uppecase font-extrabold'>Start Game</button></NavLink>
+                                        :
+                                        <NavLink to={`/battle/level01/${currentUserDataSet[1]}`} target="_blank"><button className='text-sm sm:text-sm md:text-[22px] lg:text-md w-full h-[3rem] sm:h-[3rem] md:h-[5rem] lg:h-[5rem] bg-[#371152] hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 uppecase font-extrabold'>Start Game</button></NavLink>
+                                }
+
                             </div>
                         </div>
                     </div>
                 </div>
-                {/* Level Content */}
-                <div className='flex flex-col justify-center items-center'>
-                    <span className='mt-[3rem] sm:mt-[3rem] md:mt-[5rem] lg:mt-[8rem] my-[3rem] sm:my-[3rem] md:my-[5rem] lg:my-[5rem] md:text-[1.3rem] lg:text-[1.7rem] uppercase font-extrabold text-[#B154F0]'>WHAT WILL YOU MEET IN THIS LEVEL ?</span>
-                    <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-[50px] sm:gap-[50px] lg:gap-[150px] mb-[5rem]'>
-                        <img src={M01} alt="" className='w-full max-w-[17rem] md:max-w-[20rem] lg:max-w-[35rem]' />
-                        <img src={M02} alt="" className='w-full max-w-[17rem] md:max-w-[20rem] lg:max-w-[35rem]' />
-                        <img src={M03} alt="" className='w-full max-w-[17rem] md:max-w-[20rem] lg:max-w-[35rem] ml-[1.5rem] sm:ml-[1.5rem] lg:ml-[2.9rem]' />
-                    </div>
+            </div>
+            {/* Level Content */}
+            <div className='flex flex-col justify-center items-center'>
+                <span className='mt-[3rem] sm:mt-[3rem] md:mt-[5rem] lg:mt-[8rem] my-[3rem] sm:my-[3rem] md:my-[5rem] lg:my-[5rem] md:text-[1.3rem] lg:text-[1.7rem] uppercase font-extrabold text-[#B154F0]'>WHAT WILL YOU MEET IN THIS LEVEL ?</span>
+                <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-[50px] sm:gap-[50px] lg:gap-[150px] mb-[5rem]'>
+                    <img src={M01} alt="" className='w-full max-w-[17rem] md:max-w-[20rem] lg:max-w-[35rem]' />
+                    <img src={M02} alt="" className='w-full max-w-[17rem] md:max-w-[20rem] lg:max-w-[35rem]' />
+                    <img src={M03} alt="" className='w-full max-w-[17rem] md:max-w-[20rem] lg:max-w-[35rem] ml-[1.5rem] sm:ml-[1.5rem] lg:ml-[2.9rem]' />
                 </div>
             </div>
+        </div>
     )
 }
