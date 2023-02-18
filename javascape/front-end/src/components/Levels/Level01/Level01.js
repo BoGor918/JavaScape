3f5b0e962db4b7ec04d47987cbec798a7a571e33
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2023-02-18 17:15:32 +0800

    Battle Layout Updated

--- javascape/front-end/src/components/Levels/Level01/Level01.js
+++ javascape/front-end/src/components/Levels/Level01/Level01.js
@@ -77,11 +77,6 @@ export default function Level01() {
     };
   }, [addEventListener, removeEventListener, handleGameOver]);
 
-  const LeaveGame = () => {
-    navigate("/")
-    window.location.reload()
-  }
-
   return (
     <div>
       <NavBar />
@@ -105,7 +100,7 @@ export default function Level01() {
           <div className="w-full max-w-[1280px] pt-5">
             <div className="bg-gradient-to-r from-[#FFA9C5] to-[#FF3073]/50 p-[2px] w-fit self-end">
               <div>
-                <button onClick={() => LeaveGame()} className='text-[7px] sm:text-[7px] md:text-[10px] lg:text-[16px] px-3 h-[2rem] sm:h-[2rem] md:h-[2.6rem] lg:h-[2.6rem] bg-[#371152] duration-200 hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 font-extrabold uppercase'>Leave Game</button>
+                <button onClick={() => window.close()} className='text-[7px] sm:text-[7px] md:text-[10px] lg:text-[16px] px-3 h-[2rem] sm:h-[2rem] md:h-[2.6rem] lg:h-[2.6rem] bg-[#371152] duration-200 hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 font-extrabold uppercase'>Leave Game</button>
               </div>
             </div>
           </div>
