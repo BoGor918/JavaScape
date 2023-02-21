98e54c03d1f1b2e1380070927fefdda55c052a60
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2023-02-21 19:35:40 +0800

    Adding mars loading

--- javascape/front-end/src/components/Loading.js
+++ javascape/front-end/src/components/Loading.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "../global.css"
 import Rocket from "../images/Rocket.png"
+import Mars from "../images/Mars.png"
 
 export default function Loading() {
 
     return (
         <div className="main">
+            <img className='rocket' src={Mars} alt="Rocket" />
             <div className='circle'>
                 <img className='rocket' src={Rocket} alt="Rocket" />
             </div>
