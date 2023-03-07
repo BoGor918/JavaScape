b3ce7e243a0aa764f377c968df090aea0ef191e0
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2023-03-08 00:43:11 +0800

    How to play added

--- javascape/front-end/src/components/Levels/Level02/Level02Intro.js
+++ javascape/front-end/src/components/Levels/Level02/Level02Intro.js
@@ -9,6 +9,9 @@ import { useNavigate } from 'react-router-dom'
 import { isMobile } from 'react-device-detect';
 import Loading from '../../Loading'
 import EmailVerification from '../../EmailVerification'
+import HowToPlay1 from "../../../images/Levels/Level01/HowToPlay1.png"
+import HowToPlay2 from "../../../images/Levels/Level01/HowToPlay2.png"
+import HowToPlay3 from "../../../images/Levels/Level02/HowToPlay3.png"
 
 export default function Level02Intro() {
     const {
@@ -33,6 +36,9 @@ export default function Level02Intro() {
             setLoading(false);
         }, 1500);
     }, [])
+
+    // handle display
+    const [display, setDisplay] = useState("default");
 
     return (
         <div>
@@ -71,21 +77,69 @@ export default function Level02Intro() {
                                     </div>
                                 </div>
                             </div>
-                            {/* Level Content */}
-                            <div className='flex flex-col justify-center items-center mb-[7rem] sm:mb-[7rem] md:mb-[10rem] lg:mb-[10rem]'>
-                                <span className='mt-[3rem] sm:mt-[3rem] md:mt-[5rem] lg:mt-[8rem] my-[3rem] sm:my-[3rem] md:my-[5rem] lg:my-[5rem] md:text-[1.3rem] lg:text-[1.7rem] uppercase font-extrabold text-[#B154F0]'>WHAT WILL YOU MEET IN THIS LEVEL ?</span>
-                                <div className='hidden sm:hidden md:grid lg:grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-[50px] sm:gap-x-[50px] lg:gap-x-[150px] gap-y-[50px] sm:gap-y-[50px] lg:gap-y-[110px]'>
-                                    <img src={M04} alt="" className='w-full max-w-[17rem] md:max-w-[20rem] lg:max-w-[35rem]' />
-                                    <img src={M05} alt="" className='w-full max-w-[17rem] md:max-w-[20rem] lg:max-w-[35rem]' />
-                                    <img src={M06} alt="" className='w-full max-w-[17rem] md:max-w-[20rem] lg:max-w-[35rem] ml-[1.5rem] sm:ml-[1.5rem] md:ml-[0.6rem] lg:ml-[rem]' />
-                                </div>
+                            {/* filter button */}
+                            <div className='flex justify-center items-center mt-[3rem] sm:mt-[3rem] md:mt-[5rem] lg:mt-[8rem] font-bold'>
+                                <div className="bg-gradient-to-r from-[#FFA9C5] to-[#FF3073]/50 p-[2px] my-3 max-w-[4rem] sm:max-w-[4rem] md:max-w-[7rem] lg:max-w-[7rem] w-full">
+                                    {
+                                        display === "default" ?
+                                            <button onClick={() => setDisplay("default")} className='w-full h-[2.5rem] sm:h-[2.5rem] md:h-[3rem] lg:h-[3rem] bg-[#541680] duration-200 hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 text-sm sm:text-sm md:text-[18px] lg:text-[18px] uppercase'>Level</button> :
+                                            <button onClick={() => setDisplay("default")} className='w-full h-[2.5rem] sm:h-[2.5rem] md:h-[3rem] lg:h-[3rem] bg-[#371152] duration-200 hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 text-sm sm:text-sm md:text-[18px] lg:text-[18px] uppercase'>Level</button>
+                                    }
 
-                                <div className='grid sm:grid md:hidden lg:hidden grid-cols-1 gap-[50px]'>
-                                    <img src={M04} alt="" className='w-full max-w-[17rem]' />
-                                    <img src={M05} alt="" className='w-full max-w-[17rem]' />
-                                    <img src={M06} alt="" className='w-full max-w-[17rem] ml-[0.5rem]' />
+                                </div>
+                                <div className="bg-gradient-to-r from-[#FFA9C5] to-[#FF3073]/50 p-[2px] my-3 max-w-[7rem] sm:max-w-[7rem] md:max-w-[10rem] lg:max-w-[10rem] w-full">
+                                    {
+                                        display === "howtoplay" ?
+                                            <button onClick={() => setDisplay("howtoplay")} className='w-full h-[2.5rem] sm:h-[2.5rem] md:h-[3rem] lg:h-[3rem] bg-[#541680] duration-200 hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 text-sm sm:text-sm md:text-[18px] lg:text-[18px] uppercase'>How To Play</button> :
+                                            <button onClick={() => setDisplay("howtoplay")} className='w-full h-[2.5rem] sm:h-[2.5rem] md:h-[3rem] lg:h-[3rem] bg-[#371152] duration-200 hover:bg-[#541680] border-gradient-to-br from-[#FC6DFF] to-[#9900ff]/30 text-sm sm:text-sm md:text-[18px] lg:text-[18px] uppercase'>How To Play</button>
+                                    }
+
                                 </div>
                             </div>
+                            {/* Level Content */}
+                            <>
+                                {
+
+                                    display === "default" ?
+                                        <div className='flex flex-col justify-center items-center mb-[7rem] sm:mb-[7rem] md:mb-[10rem] lg:mb-[10rem]'>
+                                            <span className='my-[3rem] sm:my-[3rem] md:my-[5rem] lg:my-[5rem] md:text-[1.3rem] lg:text-[1.7rem] uppercase font-extrabold text-[#B154F0]'>WHAT WILL YOU MEET IN THIS LEVEL ?</span>
+                                            <div className='hidden sm:hidden md:grid lg:grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-[50px] sm:gap-x-[50px] lg:gap-x-[150px] gap-y-[50px] sm:gap-y-[50px] lg:gap-y-[110px]'>
+                                                <img src={M04} alt="" className='w-full max-w-[17rem] md:max-w-[20rem] lg:max-w-[35rem]' />
+                                                <img src={M05} alt="" className='w-full max-w-[17rem] md:max-w-[20rem] lg:max-w-[35rem]' />
+                                                <img src={M06} alt="" className='w-full max-w-[17rem] md:max-w-[20rem] lg:max-w-[35rem] ml-[1.5rem] sm:ml-[1.5rem] md:ml-[0.6rem] lg:ml-[rem]' />
+                                            </div>
+
+                                            <div className='grid sm:grid md:hidden lg:hidden grid-cols-1 gap-[50px]'>
+                                                <img src={M04} alt="" className='w-full max-w-[17rem]' />
+                                                <img src={M05} alt="" className='w-full max-w-[17rem]' />
+                                                <img src={M06} alt="" className='w-full max-w-[17rem] ml-[0.5rem]' />
+                                            </div>
+                                        </div> :
+                                        <div className='w-full max-w-[21rem] sm:max-w-[21rem] md:max-w-[45rem] lg:max-w-[75rem] self-center flex flex-col justify-center items-center mb-[7rem] sm:mb-[7rem] md:mb-[10rem] lg:mb-[10rem]'>
+                                            <span className='my-[3rem] sm:my-[3rem] md:my-[5rem] lg:my-[5rem] md:text-[1.3rem] lg:text-[1.7rem] uppercase font-extrabold text-[#B154F0]'>How to Play ?</span>
+                                            <div className='flex flex-col justify-center items-center'>
+                                                <div className='flex flex-col sm:flex-col md:flex-row lg:flex-row mb-[1rem] sm:mb-[1rem] md:mb-[3rem] lg:mb-[3rem] justify-center items-center'>
+                                                    <img src={HowToPlay2} alt="" className="mt-5 sm:mt-5 md:mt-0 lg:mt-0 md:max-w-[23rem] lg:max-w-[30rem] border-[3px] rounded-2xl border-[#B154F0]" />
+                                                    <span className='md:text-[18px] md:pl-[2rem] lg:text-[23px] lg:pl-[5rem] text-justify w-full mt-3 sm:mt-3 md:mt-0 lg:mt-0'>
+                                                        This is a multiple choices question, members need to click the correct box for attacking the enemies.
+                                                    </span>
+                                                </div>
+                                                <div className='flex flex-col sm:flex-col md:flex-row lg:flex-row mt-[1rem] sm:mt-[1rem] md:mt-[3rem] lg:mt-[3rem] mb-[1rem] sm:mb-[1rem] md:mb-[3rem] lg:mb-[3rem] justify-center items-center'>
+                                                    <img src={HowToPlay1} alt="" className="mt-5 sm:mt-5 md:mt-0 lg:mt-0 md:max-w-[23rem] lg:max-w-[30rem] border-[3px] rounded-2xl border-[#B154F0]" />
+                                                    <span className='md:text-[18px] md:pl-[2rem] lg:text-[23px] lg:pl-[5rem] text-justify w-full mt-3 sm:mt-3 md:mt-0 lg:mt-0'>
+                                                        This is a fill-in-the-blank question, members need to type in the correct answer in the middle box for attacking the enemies.
+                                                    </span>
+                                                </div>
+                                                <div className='flex flex-col sm:flex-col md:flex-row lg:flex-row mt-[1rem] sm:mt-[1rem] md:mt-[3rem] lg:mt-[3rem] justify-center items-center'>
+                                                    <img src={HowToPlay3} alt="" className="mt-5 sm:mt-5 md:mt-0 lg:mt-0 md:max-w-[23rem] lg:max-w-[30rem] border-[3px] rounded-2xl border-[#B154F0]" />
+                                                    <span className='md:text-[18px] md:pl-[2rem] lg:text-[23px] lg:pl-[5rem] text-justify w-full mt-3 sm:mt-3 md:mt-0 lg:mt-0'>
+                                                        This is a drag and drop question, members need to drag the correct box in the middle box for attacking the enemies.
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                }
+                            </>
                         </div>
             }
         </div>
