21eec1ad534f037a1d151cbe6f85f95668d028db
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2023-03-08 22:37:24 +0800

    Update RankList.js

--- javascape/front-end/src/components/Rank/RankList.js
+++ javascape/front-end/src/components/Rank/RankList.js
@@ -5,6 +5,8 @@ export default function RankList({ listData, currentUserDataSet, wholeDataLength
     // navigate function
     const navigate = useNavigate()
 
+    console.log(wholeDataLength)
+
     return (
         <>
             {
@@ -67,7 +69,7 @@ export default function RankList({ listData, currentUserDataSet, wholeDataLength
                                                     <div onClick={() => navigate("/profile")} className='flex justify-between hover:bg-black/20 rounded-lg cursor-pointer'>
                                                         <div className='w-full flex justify-center'>
                                                             <span className='text-sm sm:text-sm md:text-xl lg:text-xl text-white my-2 sm:my-2 md:my-3 lg:my-3'>
-                                                                {wholeDataLength - user.index}
+                                                                {wholeDataLength - 1 - user.index}
                                                             </span>
                                                         </div>
                                                         <div className='w-full flex justify-center'>
@@ -84,7 +86,7 @@ export default function RankList({ listData, currentUserDataSet, wholeDataLength
                                                     <div onClick={() => navigate(`/profile/${user.Username}`)} className='flex justify-between hover:bg-black/20 rounded-lg cursor-pointer'>
                                                         <div className='w-full flex justify-center'>
                                                             <span className='text-sm sm:text-sm md:text-xl lg:text-xl text-white my-2 sm:my-2 md:my-3 lg:my-3'>
-                                                                {wholeDataLength - user.index}
+                                                                {wholeDataLength - 1 - user.index}
                                                             </span>
                                                         </div>
                                                         <div className='w-full flex justify-center'>
