2f059d6a83e2b18f47bd276789ffd2d47d51e426
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2023-03-11 13:15:25 +0800

    Update App.js

--- javascape/front-end/src/App.js
+++ javascape/front-end/src/App.js
@@ -1,28 +1,29 @@
-import React, { useContext } from 'react';
-import SignUp from './components/Authentication/SignUp';
-import Login from './components/Authentication/Login';
-import Profile from './components/Profile/Profile';
+import React, { useContext, lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './components/Home';
-import Level01 from './components/Levels/Level01/Level01';
-import Level01Mobile from './components/Levels/Level01/Level01_Mobile';
 import { MapperContext } from './globalVariables/MapperContextProvider';
-import Battle from './components/Battle';
-import Level01Intro from './components/Levels/Level01/Level01Intro';
-import Rank from './components/Rank/Rank';
-import AllUserProfile from './components/Profile/AllUserProfile';
-import Forum from './components/Forum/ForumComponents/Forum';
-import ForumDetail from './components/Forum/ForumDetail';
-import Topic from './components/Topic/Topic';
-import AutoResearchSystem from './components/AutoResearchSystem';
-import Compiler from './components/Topic/Compiler';
-import Shop from './components/Shop/Shop';
-import Ability from './components/Shop/Ability';
-import Loading from './components/Loading';
-import PageNotFound from './components/PageNotFound';
-import Level02Intro from './components/Levels/Level02/Level02Intro';
-import Level02 from './components/Levels/Level02/Level02';
-import Level02Mobile from './components/Levels/Level02/Level02_Mobile';
+
+// import all lazy load component
+const Login = lazy(() => import('./components/Authentication/Login'));
+const SignUp = lazy(() => import('./components/Authentication/SignUp'));
+const Rank = lazy(() => import('./components/Rank/Rank'));
+const Home = lazy(() => import('./components/Home'));
+const Battle = lazy(() => import('./components/Battle'));
+const Level01Intro = lazy(() => import('./components/Levels/Level01/Level01Intro'));
+const Level02Intro = lazy(() => import('./components/Levels/Level02/Level02Intro'));
+const Level01 = lazy(() => import('./components/Levels/Level01/Level01'));
+const Level01Mobile = lazy(() => import('./components/Levels/Level01/Level01_Mobile'));
+const Level02 = lazy(() => import('./components/Levels/Level02/Level02'));
+const Level02Mobile = lazy(() => import('./components/Levels/Level02/Level02_Mobile'));
+const Profile = lazy(() => import('./components/Profile/Profile'));
+const AllUserProfile = lazy(() => import('./components/Profile/AllUserProfile'));
+const Forum = lazy(() => import('./components/Forum/ForumComponents/Forum'));
+const ForumDetail = lazy(() => import('./components/Forum/ForumDetail'));
+const Topic = lazy(() => import('./components/Topic/Topic'));
+const AutoResearchSystem = lazy(() => import('./components/AutoResearchSystem'));
+const Compiler = lazy(() => import('./components/Topic/Compiler'));
+const Shop = lazy(() => import('./components/Shop/Shop'));
+const Ability = lazy(() => import('./components/Shop/Ability'));
+const PageNotFound = lazy(() => import('./components/PageNotFound'));
 
 export default function App() {
   // call data from mapper context js
@@ -35,52 +36,53 @@ export default function App() {
 
   return (
     <Router>
-      <Routes>
-        {/* common link */}
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/battle" element={<Battle />} />
-        <Route path="/rank" element={<Rank />} />
-        <Route path="/forum" element={<Forum />} />
-        <Route path="/autoresearch" element={<AutoResearchSystem />} />
-        <Route path="/compiler" element={<Compiler />} />
-        <Route path="/topic" element={<Topic />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/ability" element={<Ability />} />
-        <Route path="/loading" element={<Loading />} />
-        <Route path="*" element={<PageNotFound />} />
-        {/* levels link */}
-        <Route path="/battle/level01" element={<Level01Intro />} />
-        <Route path="/battle/level02" element={<Level02Intro />} />
-        {/* gameplay link route */}
-        {
-          authUser !== null ?
-            <>
-              <Route path={"/battle/level01/" + currentUserDataSet[1]} element={<Level01 />} />
-              <Route path={"/battle/level01/" + currentUserDataSet[1] + "/mobile"} element={<Level01Mobile />} />
-              <Route path={"/battle/level02/" + currentUserDataSet[1]} element={<Level02 />} />
-              <Route path={"/battle/level02/" + currentUserDataSet[1] + "/mobile"} element={<Level02Mobile />} />
-            </> : <></>
-        }
-        {/* all User Profile Link */}
-        {
-          userData.map((user, i) => {
-            return (
-              <Route key={i} path={"/profile/" + user.Username} element={<AllUserProfile />} />
-            )
-          })
-        }
-        {/* all forum link */}
-        {
-          forumData.map((forum, i) => {
-            return (
-              <Route key={i} path={"/forum/" + forum.id} element={<ForumDetail />} />
-            )
-          })
-        }
-      </Routes>
+      <Suspense fallback={<></>}>
+        <Routes>
+          {/* common link */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/battle" element={<Battle />} />
+          <Route path="/rank" element={<Rank />} />
+          <Route path="/forum" element={<Forum />} />
+          <Route path="/autoresearch" element={<AutoResearchSystem />} />
+          <Route path="/compiler" element={<Compiler />} />
+          <Route path="/topic" element={<Topic />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/ability" element={<Ability />} />
+          <Route path="*" element={<PageNotFound />} />
+          {/* levels link */}
+          <Route path="/battle/level01" element={<Level01Intro />} />
+          <Route path="/battle/level02" element={<Level02Intro />} />
+          {/* gameplay link route */}
+          {
+            authUser !== null ?
+              <>
+                <Route path={"/battle/level01/" + currentUserDataSet[1]} element={<Level01 />} />
+                <Route path={"/battle/level01/" + currentUserDataSet[1] + "/mobile"} element={<Level01Mobile />} />
+                <Route path={"/battle/level02/" + currentUserDataSet[1]} element={<Level02 />} />
+                <Route path={"/battle/level02/" + currentUserDataSet[1] + "/mobile"} element={<Level02Mobile />} />
+              </> : <></>
+          }
+          {/* all User Profile Link */}
+          {
+            userData.map((user, i) => {
+              return (
+                <Route key={i} path={"/profile/" + user.Username} element={<AllUserProfile />} />
+              )
+            })
+          }
+          {/* all forum link */}
+          {
+            forumData.map((forum, i) => {
+              return (
+                <Route key={i} path={"/forum/" + forum.id} element={<ForumDetail />} />
+              )
+            })
+          }
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
